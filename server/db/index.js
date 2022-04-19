88068b5d4a52d7ff6d9f1a35346098e6d4d4bfb2
jtshivers <jacobshivers@protonmail.com>
2022-04-19 16:28:31 -0500

    Added models, associations and seed data

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,26 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
+const Sequelize = require("sequelize");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Product = require("./models/Product");
 
-//associations could go here!
+const User_Product = db.define("User_Product", {
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
+    },
+  },
+});
+Product.belongsToMany(User, { through: User_Product });
+User.belongsToMany(Product, { through: User_Product });
 
 module.exports = {
   db,
   models: {
     User,
+    Product,
   },
-}
+};
