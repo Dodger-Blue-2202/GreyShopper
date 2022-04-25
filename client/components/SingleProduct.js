c622a22acfb7174afceabbd27848561c14e070d5
Mike <lebegue92@yahoo.com>
2022-04-25 10:26:26 -0500

    Connected Product and SingleProducted to Store

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,40 +1,40 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { fetchSingleProduct } from '../store/productsReducer'
 
-const Product = (props) => {
-	const product = props.match
-	console.log(props)
-	return (
-		<div className="product">
-			<div className="row align-items-center">
-				<h4>Name: {product.name}</h4>
-				<h4>Price: ${product.price / 100}</h4>
-				<h4>Description: {product.description}</h4>
-				<h4>Quantity: {product.stock}</h4>
+export class SingleProduct extends React.Component {
+	componentDidMount() {
+		this.props.fetchSingleProduct(this.props.match.params.id)
+	}
+	render() {
+		let product = this.props.product
+		if (product === undefined) {
+			product = ''
+		}
+		return (
+			<div className="container-fluid" key={product.id}>
+				<div className="row align-items-start">
+					<img className="product-image" src={product.imageUrl} />
+				</div>
+				<div className="product" key={product.id}>
+					<div className="row align-items-center">
+						<h4>Name: {product.name}</h4>
+						<h4>Price: ${product.price / 100}</h4>
+						<h4>Description: {product.description}</h4>
+						<h4>Quantity: {product.stock}</h4>
+					</div>
+				</div>
 			</div>
-		</div>
-
-		// <div>
-		// 	<ul>
-		// 		{products.map((product) => (
-		// 			<li key={product.id}>
-		// 				<div>
-		// 					<Link to={`/products/${product.id}`}>
-		// 						<img src={product.imageUrl} />
-		// 					</Link>
-		// 				</div>
-		// 				<h2>{product.name}</h2>
-		// 				<h2>{product.price}</h2>
-		// 				<h2>{product.description}</h2>
-		// 				<h2>{product.stock}</h2>
-		// 			</li>
-		// 		))}
-		// 	</ul>
-		// </div>
-	)
+		)
+	}
 }
 
-const mapState = (state) => ({ products: state.products })
+const mapState = (state) => ({ product: state.products })
 
-export default connect(mapState)(Product)
+const mapDispatch = (dispatch) => {
+	return {
+		fetchSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
+	}
+}
+
+export default connect(mapState, mapDispatch)(SingleProduct)
