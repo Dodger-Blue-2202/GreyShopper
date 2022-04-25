464b3fc27b47f7783d416cf3ff8d93cc2a29ac70
mc5368 <51121685+mc5368@users.noreply.github.com>
2022-04-25 11:53:42 -0400

    Update Navbar.js

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -25,35 +25,3 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
 
 export default Navbar //connect(mapState, mapDispatch)(Navbar)
 
-// {isLoggedIn ? (
-//   <div>
-//     {/* The navbar will show these links after you log in */}
-//     <Link to="/home">Home</Link>
-//     <a href="#" onClick={handleClick}>
-//       Logout
-//     </a>
-//   </div>
-// ) : (
-//   <div>
-//     {/* The navbar will show these links before you log in */}
-//     <Link to="/login">Login</Link>
-//     <Link to="/signup">Sign Up</Link>
-//   </div>
-// )}
-
-// /**
-//  * CONTAINER
-//  */
-// const mapState = state => {
-//   return {
-//     isLoggedIn: !!state.auth.id
-//   }
-// }
-
-// const mapDispatch = dispatch => {
-//   return {
-//     handleClick() {
-//       dispatch(logout())
-//     }
-//   }
-// }
