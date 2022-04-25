a7d31d3057efafac54f1717508e1cca328061d88
Parry <pt987@nyu.edu>
2022-04-25 17:42:14 -0400

    added users page for admins

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Login, Signup } from './AuthForm'
+import React from "react";
+import { connect } from "react-redux";
+import Products from "./Products";
+import { Login, Signup } from "./AuthForm";
 
 /**
  * COMPONENT
  */
 export const Home = (props) => {
-	const { username } = props
+  const { username } = props;
 
-	return (
-		<div>
-			<h3>Welcome, {username}</h3>
-			<Products />
-			{/* <Signup />
+  return (
+    <div>
+      <h3>Welcome, {username}</h3>
+      <Products />
+      {/* <Signup />
       <Login /> */}
-		</div>
-	)
-}
+    </div>
+  );
+};
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-	return {
-		username: state.auth.username,
-		isAdmin: state.auth.isAdmin,
-	}
-}
+  return {
+    username: state.auth.username,
+    isAdmin: state.auth.isAdmin,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
